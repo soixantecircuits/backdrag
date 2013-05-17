6df1930f62fe2382e2f29c0f9e990492c7510796
Gabriel Delattre <gabriel@soixantecircuits.fr>
2013-05-18 00:13:11 +0200

    adding debug infos

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@
       var $el = this.find(opt.handle);
     }
 
-    return $el.css('cursor', opt.cursor).on("mousedown touchstart", function(e) {
+    return $el.css('cursor', opt.cursor).bind("mousedown touchstart", function(e) {
       if (opt.handle === "") {
         var $drag = $(this).prev().addClass('draggable');
       } else {
@@ -23,19 +23,19 @@
         drg_w = $drag.outerWidth(),
         pos_y = $drag.offset().top + drg_h - e.pageY,
         pos_x = $drag.offset().left + drg_w - e.pageX;
-      $drag.css('z-index', -1).parents().on("mousemove touchmove", function(e) {
+      $drag.css('z-index', -1).parents().bind("mousemove touchmove", function(e) {
         var topPosition = e.pageY + pos_y - drg_h,
           leftPosition = e.pageX + pos_x - drg_w;
         console.log(topPosition, ':', leftPosition);
         $('.draggable').offset({
           top: topPosition,
           left: leftPosition
-        }).on("mouseup touchend", function() {
+        }).bind("mouseup touchend", function() {
           $(this).prev().removeClass('draggable').css('z-index', z_idx);
         });
       });
       e.preventDefault(); // disable selection
-    }).on("mouseup touchend", function() {
+    }).bind("mouseup touchend", function() {
       if (opt.handle === "") {
         $(this).prev().removeClass('draggable');
       } else {
@@ -45,12 +45,7 @@
 
   }
 })(jQuery);
-$(document).bind('touchmove', function(event) {
-  var orig = event.originalEvent;
-  var x = orig.changedTouches[0].pageX;
-  var y = orig.changedTouches[0].pageY;
-  alert("x:" + x + "y:" + y);
-});
+
 $("#image1").click(function(e) {
   var parentOffset = $(this).parent().offset();
   //or $(this).offset(); if you really just want the current element's offset
